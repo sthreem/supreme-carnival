78ad4b90661484afcab8cc04884ff2a923fe7c32
Stefan Milivojevic <milivojevic.ste@gmail.com>
2023-04-07 14:09:55 -0400

    Implemented home nav

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -1,7 +1,115 @@
 <template>
-  <h1>Home Nav</h1>
+  <nav class="nav">
+    <ul class="nav__list">
+      <li
+        v-for="(navItem, index) in navItems"
+        :key="index"
+        class="list__item"
+        :class="{ active: navItem.name === currentRouteName }"
+      >
+        <router-link :to="{ name: navItem.name }" class="item__link">
+          <div class="link__icon">
+            <img :src="navItem.icon" :alt="navItem.name" />
+          </div>
+          <div class="link__name">
+            {{ navItem.name }}
+          </div>
+        </router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, computed } from 'vue'
+import { useRoute } from 'vue-router'
 
-<style lang="scss" scoped></style>
+import AnalyticsIcon from '@/assets/icons/home-nav/Analytics.svg'
+import OffersIcon from '@/assets/icons/home-nav/Discounts.svg'
+import LoyaltyIcon from '@/assets/icons/home-nav/Loyalty.svg'
+import CurrenciesIcon from '@/assets/icons/home-nav/Currencies.svg'
+import DispatchIcon from '@/assets/icons/home-nav/Driver.svg'
+import ConfigurationsIcon from '@/assets/icons/home-nav/Settings.svg'
+
+const navItems = ref([
+  {
+    name: 'Analytics',
+    icon: AnalyticsIcon,
+  },
+  {
+    name: 'Offers',
+    icon: OffersIcon,
+  },
+  {
+    name: 'Loyalty',
+    icon: LoyaltyIcon,
+  },
+  {
+    name: 'Currencies',
+    icon: CurrenciesIcon,
+  },
+  {
+    name: 'Dispatch',
+    icon: DispatchIcon,
+  },
+  {
+    name: 'Configurations',
+    icon: ConfigurationsIcon,
+  },
+])
+
+const route = useRoute()
+
+const currentRouteName = computed(() => {
+  return route.name
+})
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  width: 100%;
+  height: 100%;
+
+  &__list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+
+.list {
+  &__item {
+    width: 100%;
+  }
+}
+
+.item {
+  &__link {
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    color: #333;
+    text-decoration: none;
+  }
+}
+
+.link {
+  &__icon {
+    margin-right: 1rem;
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  &__name {
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+}
+
+.active {
+  background-color: #fff;
+}
+</style>
